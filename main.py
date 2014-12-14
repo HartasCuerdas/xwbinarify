bef08c1989e6304fd3c2fa2605bb6b5b27f5c91a
Franco Cedillo <franco.cedillo@gmail.com>
2014-12-14 17:14:09 -0500

    adds rows

--- main.py
+++ main.py
@@ -2,9 +2,12 @@ import numpy as np
 import cv2
 
 img = cv2.imread('crossword.jpg')
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-ret, thresh = cv2.threshold(gray,127,255,cv2.THRESH_BINARY_INV)
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+ret, thresh = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY_INV)
 thresh2 = cv2.bitwise_not(thresh)
+
+rows = 13
+cols = 13
 
 contours,hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, 1)
 
@@ -12,7 +15,7 @@ max_area = -1
 
 # find contours with maximum area
 for cnt in contours:
-    approx = cv2.approxPolyDP(cnt, 0.02*cv2.arcLength(cnt,True), True)
+    approx = cv2.approxPolyDP(cnt, 0.02*cv2.arcLength(cnt, True), True)
     if len(approx) == 4:
         if cv2.contourArea(cnt) > max_area:
             max_area = cv2.contourArea(cnt)
@@ -22,7 +25,7 @@ for cnt in contours:
 # cut the crossword region, and resize it to a standard size of 130x130
 x,y,w,h = cv2.boundingRect(max_cnt)
 cross_rect = thresh2[y:y+h, x:x+w]
-cross_rect = cv2.resize(cross_rect,(130,130))
+cross_rect = cv2.resize(cross_rect,(rows*10, cols*10))
 
 # you need to uncomment these lines if your image is rotated
 #new_pts = np.float32([[0,0], [0,129],[129,129],[129,0]])
@@ -30,13 +33,13 @@ cross_rect = cv2.resize(cross_rect,(130,130))
 #M = cv2.getPerspectiveTransform(old_pts,new_pts)
 #cross_rect = cv2.warpPerspective(thresh2,M,(130,130))
 
-cross = np.zeros((13,13))
+cross = np.zeros((rows, cols))
 
 # select each box, if number of white pixels is more than 50, it is white box
-for i in xrange(13):
-    for j in xrange(13):
+for i in xrange(rows):
+    for j in xrange(cols):
         box = cross_rect[i*10:(i+1)*10, j*10:(j+1)*10]
         if cv2.countNonZero(box) > 50:
-            cross.itemset((i,j),1)
+            cross.itemset((i,j) ,1)
 
 print cross
